8e649e3f3938a42b3c7d48c3f2a9dff8f366cae6
aroasanight <vxcqpvyl3@2bpencil.mozmail.com>
2026-03-30 17:36:34 +0100

    unit tests for setter functions

---
+++ test.py
@@ -0,0 +1,134 @@
+import unittest
+from main import *
+
+# unit testing derived from https://www.geeksforgeeks.org/python/unit-testing-python-unittest/
+
+class TestConfigClass(unittest.TestCase):
+
+    def setUp(self):
+        if os.path.exists("test_config.json"):
+            os.remove("test_config.json")
+
+
+
+    # sample rate
+
+    def test_valid_sr(self):
+        c = Config(path="test_config.json")
+        c.set_sr(44100)
+        self.assertEqual(c.sr, 44100)
+
+    def test_valid_sr_lower_bound(self):
+        c = Config(path="test_config.json")
+        c.set_sr(8000)
+        self.assertEqual(c.sr, 8000)
+
+    def test_valid_sr_upper_bound(self):
+        c = Config(path="test_config.json")
+        c.set_sr(192000)
+        self.assertEqual(c.sr, 192000)
+
+    def test_invalid_sr(self):
+        c = Config(path="test_config.json")
+        with self.assertRaises(ValueError):
+            c.set_sr(12345)
+
+    def test_invalid_sr_format(self):
+        c = Config(path="test_config.json")
+        with self.assertRaises((TypeError, ValueError)):
+            c.set_sr("according to all known laws of aviation, there is no way that the bee should be able to fly.")
+
+
+
+    # buffer
+
+    def test_valid_buf(self):
+        c = Config(path="test_config.json")
+        c.set_buf(500)
+        self.assertEqual(c.buf, 500)
+
+    def test_valid_buf_lower_bound(self):
+        c = Config(path="test_config.json")
+        c.set_buf(0)
+        self.assertEqual(c.buf, 0)
+
+    def test_invalid_buf_lower_bound(self):
+        c = Config(path="test_config.json")
+        with self.assertRaises(ValueError):
+            c.set_buf(-100)
+
+    def test_invalid_buf_format(self):
+        c = Config(path="test_config.json")
+        with self.assertRaises((TypeError, ValueError)):
+            c.set_buf("according to all known laws of aviation, there is no way that the bee should be able to fly.")
+
+
+
+    # gain
+
+    def test_valid_gain(self):
+        c = Config(path="test_config.json")
+        c.set_gain(1.5)
+        self.assertEqual(c.gain, 1.5)
+
+    def test_valid_gain_upper_bound(self):
+        c = Config(path="test_config.json")
+        c.set_gain(8.0)
+        self.assertEqual(c.gain, 8.0)
+
+    def test_invalid_gain_lower_bound(self):
+        c = Config(path="test_config.json")
+        with self.assertRaises(ValueError):
+            c.set_gain(0.0)
+
+    def test_invalid_gain_upper_bound(self):
+        c = Config(path="test_config.json")
+        with self.assertRaises(ValueError):
+            c.set_gain(9.0)
+
+    def test_invalid_gain_format(self):
+        c = Config(path="test_config.json")
+        with self.assertRaises((TypeError, ValueError)):
+            c.set_gain("according to all known laws of aviation, there is no way that the bee should be able to fly.")
+
+
+
+    # port
+
+    def test_valid_port(self):
+        c = Config(path="test_config.json")
+        c.set_port(5006)
+        self.assertEqual(c.port, 5006)
+
+    def test_valid_port_lower_bound(self):
+        c = Config(path="test_config.json")
+        c.set_port(1024)
+        self.assertEqual(c.port, 1024)
+
+    def test_valid_port_upper_bound(self):
+        c = Config(path="test_config.json")
+        c.set_port(65535)
+        self.assertEqual(c.port, 65535)
+
+    def test_invalid_port_lower_bound(self):
+        c = Config(path="test_config.json")
+        with self.assertRaises(ValueError):
+            c.set_port(80)
+
+    def test_invalid_port_upper_bound(self):
+        c = Config(path="test_config.json")
+        with self.assertRaises(ValueError):
+            c.set_port(1189998819991197253)
+
+    def test_invalid_port_format(self):
+        c = Config(path="test_config.json")
+        with self.assertRaises((TypeError, ValueError)):
+            c.set_port("according to all known laws of aviation, there is no way that the bee should be able to fly.")
+
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
